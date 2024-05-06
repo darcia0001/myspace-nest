bc4dbefd62606b18bbe7b4363452bae730cbc329
darcia <darcia0001@gmail.com>
2024-05-06 19:00:31 +0000

    init project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WelcomeController } from './welcome.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, WelcomeController],
   providers: [AppService],
 })
 export class AppModule {}
